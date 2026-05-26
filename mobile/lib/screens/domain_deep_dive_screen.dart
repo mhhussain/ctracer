86ef658bcb19254b9dc790938e2e5debf45c1adf
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2026-05-26 00:24:16 -0400

    feat: add GoRouter shell with bottom nav and dark theme

--- mobile/lib/screens/domain_deep_dive_screen.dart
+++ mobile/lib/screens/domain_deep_dive_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class DomainDeepDiveScreen extends StatelessWidget {
-  const DomainDeepDiveScreen({super.key});
+  final String domainId;
+  const DomainDeepDiveScreen({super.key, required this.domainId});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Domain Deep Dive')));
+    return Scaffold(body: Center(child: Text('Domain Deep Dive: $domainId')));
   }
 }
